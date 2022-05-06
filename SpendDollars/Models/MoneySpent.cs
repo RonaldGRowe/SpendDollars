baaf4191bca39090e54d00bc01b6bfed5af6cf34
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-05-06 08:52:14 -0500

    Add project files.

---
+++ SpendDollars/Models/MoneySpent.cs
@@ -0,0 +1,17 @@
+﻿namespace SpendDollars.Models
+{
+    public class MoneySpent
+    {
+        public int ID { get; set; }
+        public decimal EatOut { get; set; }
+        public decimal Vacation { get; set; }
+        public decimal Entertainment { get; set; }
+        public decimal Education { get; set; }
+        public decimal Shopping { get; set; }
+        public decimal Party { get; set; }
+        public decimal Donate { get; set; }
+        public decimal Spa { get; set; }
+        public decimal Hobby { get; set; }
+        public decimal Personal { get; set; }
+    }
+}
